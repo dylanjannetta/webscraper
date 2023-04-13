b7836d016019bf07ffef13130d43c65f98aef063
Dylan Jannetta <dylanjannetta@hotmail.co.uk>
2023-04-13 21:54:16 +0100

    reformatted code to comply with prettier format

--- webscraper.py
+++ webscraper.py
@@ -9,20 +9,26 @@ soup = BeautifulSoup(website.text, 'html.parser')
 links = soup.select('.titleline > a')
 subtext = soup.select('.subtext')
 
+
 def sort_jobs_by_age(jobslist):
     return sorted(jobslist, key=lambda k: k['age'], reverse=True)
+
 
 def create_custom_joblist(links, subtext):
     joblist = []
     for idx, item in enumerate(links):
         title = item.getText()
         href = item.get('href', None)
-        post = subtext[idx].select('.age')                  # returns a list
-        post_age = post[0].get_text()                       # returns the string such as "5 days ago"
-        post_age_i = int(post_age[0:post_age.find(' ')])    # returns an int of how many days such as 5
+        # return a list
+        post = subtext[idx].select('.age')
+        # return the string such as "5 days ago"
+        post_age = post[0].get_text()
+        # return an int of how many days such as 5
+        post_age_i = int(post_age[0:post_age.find(' ')])
 
         if len(post) and post_age_i <= 4:
-            joblist.append({'title': title, 'link': href, 'post_age': post_age_i})
+            joblist.append(
+                {'title': title, 'link': href, 'post_age': post_age})
 
     return joblist
 
