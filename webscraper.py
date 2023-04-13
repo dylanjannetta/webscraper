2f39e7c8e1f7244b503c92507d250ea0881c613f
dylanjannetta <dylanjannetta@hotmail.co.uk>
2023-04-13 19:53:05 +0100

    now only shows posts > 4 days old

--- webscraper.py
+++ webscraper.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
+# look up regular expressions
+
 website = requests.get('https://news.ycombinator.com/jobs', timeout=10)
 soup = BeautifulSoup(website.text, 'html.parser')
 links = soup.select('.titleline > a')
@@ -15,12 +17,15 @@ def create_custom_joblist(links, subtext):
     for idx, item in enumerate(links):
         title = item.getText()
         href = item.get('href', None)
-        post = subtext[idx].select('.age')
-        if len(post):
-            points = str(post[0].getText())
-            joblist.append({'title': title, 'link': href, 'post': points})
+        post = subtext[idx].select('.age')                  # returns a list
+        post_age = post[0].get_text()                       # returns the string such as "5 days ago"
+        post_age_i = int(post_age[0:post_age.find(' ')])    # returns an int of how many days such as 5
+
+        if len(post) and post_age_i <= 4:
+            joblist.append({'title': title, 'link': href, 'post_age': post_age_i})
+
     return joblist
 
 
-pprint.pprint(create_custom_joblist(links, subtext))
-
+job_list = create_custom_joblist(links, subtext)
+pprint.pprint(job_list)
